635c3814cd1ffda9315a5c1e217889e3d01738a2
SotirisStyl <sstylianou2@uclan.ac.uk>
2025-02-12 22:06:59 +0200

    Created the main page, the sign up page. the sign in page, a simple main page that shows the quizzes without all the feature yet, a page to customize the profile picture, a page to show the leaderboard and a page to show the profile of the user with the ability to log out

---
+++ lib/Quiz Easy/easy_main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EasyMainPage extends StatelessWidget {
+  const EasyMainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Easy Mode Quiz Page')));
+  }
+}
